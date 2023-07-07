f0ade5930fb2b5d5d0bf22e4b4d1e7f84f39d5cf
Skylar Bruce <84935503+skylovescoffee@users.noreply.github.com>
2023-07-07 16:25:06 -0400

    Update telescope.lua - update layout config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,8 +2,8 @@ require('telescope').setup{
   defaults = {
     path_display = { "shorten" }, -- truncate, absolute, shorten
     layout_config = {
-      preview_width = 0.4,  -- Adjust as needed
-      results_width = 0.6,  -- Adjust as needed
+      preview_width = 0.6,  -- Adjust as needed
+      results_width = 0.4,  -- Adjust as needed
     },
   },
 }
