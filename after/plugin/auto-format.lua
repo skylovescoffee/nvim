14b18337ca7b98d4d8f9d2897e616cb0577898f4
Skylar Bruce <84935503+skylovescoffee@users.noreply.github.com>
2023-07-12 17:51:54 -0400

    Add css auto formatting

--- after/plugin/auto-format.lua
+++ after/plugin/auto-format.lua
@@ -1,9 +1,16 @@
+-- https://github.com/vim-autoformat/vim-autoformat/blob/master/plugin/defaults.vim
+
+
+-- HTML formatters
 vim.g["formatdef_htmlbeautify"] = "'html-beautify -f -'"
 vim.g["formatters_html"] = {"htmlbeautify"}
 
+-- CSS formatters
+-- Just using defaults for this one
+
 vim.cmd([[augroup fmt
   autocmd!
-  autocmd BufWritePre *.html Autoformat
+  autocmd BufWritePre *.{html,css} Autoformat
 augroup END]])
 
 
--- lua/skylovescoffee/keybindings.lua
+++ lua/skylovescoffee/keybindings.lua
@@ -1,10 +1,17 @@
 vim.g.mapleader = " "
-
-vim.api.nvim_set_keymap('n', '<leader>e', ':Explore<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>c', 'ciw', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>e', ':Explore<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>c', 'ciw', { noremap = true, silent = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+-- This code should be placed in your init.lua file, or a separate Lua file that you source from init.lua.
+vim.api.nvim_set_keymap('n', '<Leader>yy', ':lua YankBufferToClipboard()<CR>', { noremap = true, silent = true })
+
+function YankBufferToClipboard()
+  -- The following command will yank all lines in the buffer
+  vim.cmd('%y+')
+end
