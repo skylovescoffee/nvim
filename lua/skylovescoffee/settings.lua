c5a97c87ac6cb76465d9076ef3b149fd7cd87faf
Skylar Bruce <84935503+skylovescoffee@users.noreply.github.com>
2023-07-22 22:24:12 -0400

    Add comments to settings

--- lua/skylovescoffee/settings.lua
+++ lua/skylovescoffee/settings.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = ' '
+
+-- Window
 vim.wo.number = true  -- Set current line number
 vim.wo.relativenumber = true  -- Set relative line numbers
+
+-- Options
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -9,3 +13,5 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.scrolloff = 8
+
+
