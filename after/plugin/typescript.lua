0a0c5f20f57d201066d6d90cee77017135389649
Skylar Bruce <84935503+skylovescoffee@users.noreply.github.com>
2023-07-25 14:37:30 -0400

    Add default values to typescript plugin

--- after/plugin/typescript.lua
+++ after/plugin/typescript.lua
@@ -4,9 +4,38 @@ require("typescript").setup({
     go_to_source_definition = {
         fallback = true, -- fall back to standard LSP definition on failure
     },
-    server = { -- pass options to lspconfig's setup method
-        on_attach = ...,
+    server = {
+    on_attach = function(client, bufnr)
+      -- your other on_attach stuff here if you have any
+      -- ...
+      vim.lsp.buf.inlay_hint(bufnr, true)
+    end,
+    settings={
+      -- specify some or all of the following settings if you want to adjust the default behavior
+      javascript = {
+        inlayHints = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+        },
+      },
+      typescript = {
+        inlayHints = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = true,
+        },
+      },
     },
+  },
 })
 
 
