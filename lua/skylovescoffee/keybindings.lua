55aacc8de0edada0e62674e25f57b88f2b1769ae
Skylar Bruce <84935503+skylovescoffee@users.noreply.github.com>
2023-07-25 14:37:39 -0400

    Add search highlight remove on esc

--- lua/skylovescoffee/keybindings.lua
+++ lua/skylovescoffee/keybindings.lua
@@ -3,15 +3,11 @@ vim.api.nvim_set_keymap('n', '<leader>e', ':Explore<CR>', { noremap = true, sile
 vim.api.nvim_set_keymap('n', '<leader>c', 'ciw', { noremap = true, silent = true })
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-
--- vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format)
-
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
 -- paste without yanking deleted word
 vim.keymap.set("x", "p", [["_dP]])
 
--- This code should be placed in your init.lua file, or a separate Lua file that you source from init.lua.
 vim.api.nvim_set_keymap('n', '<Leader>yy', ':lua YankBufferToClipboard()<CR>', { noremap = true, silent = true })
 
 function YankBufferToClipboard()
@@ -19,8 +15,11 @@ function YankBufferToClipboard()
   vim.cmd('%y+')
 end
 
-
 -- Easy motion, using "f" key inspired by vim-browser
 vim.api.nvim_set_keymap('n', '<leader><leader>', ':HopWord<CR>', { noremap = true, silent = true })
 
+-- Hover
+vim.keymap.set("n", "<leader>d", ":lua vim.lsp.buf.hover()<CR>")
 
+-- Remove search highlighting on escape
+vim.keymap.set("n", "<esc>", "<esc>:noh<CR><esc>")
