b4da3786bc6350e54103b0f33bb8c6a40f60f9c0
Skylar Bruce <84935503+skylovescoffee@users.noreply.github.com>
2025-02-12 13:08:09 -0500

    Add clean lazy package management and tokyo night

--- lua/skylovescoffee/init.lua
+++ lua/skylovescoffee/init.lua
@@ -1,6 +1,12 @@
+
+-- Load initial configurations
 require("skylovescoffee.keybindings")
 require("skylovescoffee.settings")
--- require("skylovescoffee.theme")
+
+-- Initialize package manager after vim is ready
+require("skylovescoffee.lazy_init").setup()
+
+-- Load other configurations
 
 -- Call the function on every buffer read or file open event
 -- vim.cmd [[autocmd BufReadPost,BufNewFile * lua require('skylovescoffee.prettier').set_tab_width_from_prettier()]]
---
+++ lua/skylovescoffee/plugins/init.lua
@@ -0,0 +1,3 @@
+return {
+    require("skylovescoffee.plugins.tokyonight"),
+} 
